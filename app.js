d4978dcee49879ff99d6ed15efba942b563c39e4
Audra <73372336+IamAudra@users.noreply.github.com>
2020-12-18 00:42:27 -0500

    added new files and modified tailwind.config.js file

---
+++ app.js
@@ -0,0 +1,27 @@
+const regForm = document.getElementById('registration-form')
+
+const regname = document.getElementById('regname')
+const regnameErr = regname.nextElementSibling
+const email = document.getElementById('email')
+const emailErr = email.nextElementSibling
+const subject = document.getElementById('subject')
+const subjectErr = subject.nextElementSibling
+const comment = document.getElementById('comment')
+const commentErr = comment.nextElementSibling
+
+regForm.addEventListener('click', function (e) {
+  e.preventDefault()
+if (regname.value === '') {
+ regnameErr.className = 'bg-gray-400 text-white'
+} else {
+ console.log ('sending to server')
+}
+
+
+   if  (email.value ==='') {
+       emailErr.className = 'bg-gray-400 text-white'
+   } else {
+       console.log ('sending to server')
+   } 
+
+})

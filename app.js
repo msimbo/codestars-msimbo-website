4680223b0c537b183289a951cd229cf73a1c25db
Audra <73372336+IamAudra@users.noreply.github.com>
2020-12-18 01:11:47 -0500

    Added Success message validations to all sections

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const regForm = document.getElementById('registration-form')
 
 const regname = document.getElementById('regname')
 const regnameErr = regname.nextElementSibling
+const regnameSucc = regnameErr.nextElementSibling
 const email = document.getElementById('email')
 const emailErr = email.nextElementSibling
 const subject = document.getElementById('subject')
@@ -9,12 +10,13 @@ const subjectErr = subject.nextElementSibling
 const comment = document.getElementById('comment')
 const commentErr = comment.nextElementSibling
 
+
 regForm.addEventListener('click', function (e) {
   e.preventDefault()
 if (regname.value === '') {
  regnameErr.className = 'bg-gray-400 text-white'
 } else {
- console.log ('sending to server')
+ regnameSucc.className('sending to server')
 }
 
 
